befe7696a422a848aafd1c762a380e80e9e3ec4f
Tomer <tomera.almog9@gmail.com>
2025-01-14 21:52:46 +0200

    add some future functions

--- js/main.js
+++ js/main.js
@@ -11,3 +11,29 @@ function getYouTubeVideos() {
 getYouTubeVideos()
   .then((res) => console.log(res))
   .catch((err) => console.log('error: ', err))
+
+gVideos = {
+  video1: { title: 'title', embedTag: 'tag', thumbnail: 'thumbnail_url' },
+}
+
+function onPlayVideo() {
+  playVideo('tag')
+}
+
+function playVideo(tag) {
+  console.log('playing video with this tag: ', tag)
+  //play embeded youtube video clip
+}
+
+function onSearchInput(value) {
+  getTopFiveSearch(value)
+    .then(renderYouTubeSearches)
+    .catch((err) => console.log('error: ', err))
+  getTopThreeWikiSearch(value)
+    .then(renderWikiSearches)
+    .catch((err) => console.log('error: ', err))
+}
+
+function getTopFiveSearch(value) {}
+
+function getTopThreeWikiSearch(value) {}
